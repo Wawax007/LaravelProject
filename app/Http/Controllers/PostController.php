d503f0390c9052d5224603dfc6b96e874b4d1e25
Alexis <alexis.borteyru@gmail.com>
2022-07-01 16:22:21 +0200

    initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stars;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+
+    public function index(){
+        return view('index', [
+            'stars' => Stars::all()
+        ]);
+    }
+
+    public function addStars(Request $request){
+        $stars = new Stars();
+        $stars->Nom=$request->Nom;
+        $stars->Prenom=$request->Prenom;
+        $stars->Image=$request->Image;
+        $stars->Description=$request->Description;
+        if($stars->save()) {
+            echo "La star a été enregistré !";
+        }return redirect('index.php');
+    }
+}
