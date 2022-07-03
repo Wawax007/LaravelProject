7a92ec723d4889925996764cf2beaa4147aff4ca
Alexis <alexis.borteyru@gmail.com>
2022-07-03 18:31:21 +0200

    Save 3 place items

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,6 @@
 <a href="addStars">CLICK HERE</a>>
 @if($stars)
     @foreach($stars as $star) <!-- Affichage Stars -->
-
         <div id="accordion-open" data-accordion="open">
             <h2 id="accordion-open-heading-1">
                 <button type="button" class="flex items-center focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 justify-between p-5 w-full font-medium text-left border border-gray-200 dark:border-gray-700 border-b-0 text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-t-xl" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
@@ -31,10 +30,10 @@
                     <svg data-accordion-icon class="w-6 h-6 shrink-0 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </h2>
-            <div id="accordion-open-body-1" aria-labelledby="accordion-open-heading-1">
-                <img src="../public/img/{{ $star['Image'] }}">
-                <div class="p-5 border border-gray-200 dark:border-gray-700 dark:bg-gray-900 border-b-0">
-                    <p class="mb-2 text-gray-500 dark:text-gray-400"> {{ $star['Description'] }}</p>
+            <div id="accordion-open-body-2" aria-labelledby="accordion-open-heading-2">
+                <div class="p-5 border border-gray-200 content dark:border-gray-700 dark:bg-gray-900 border-b-0">
+                    <img class="object-scale-down h-48 w-96 md:object-scale-down" src="../public/img/{{ $star['Image'] }}" alt="star">
+                    <p class="mt-2 text-gray-500 text-justify float-right dark:text-gray-400"> {{ $star['Description'] }}</p>
                 </div>
             </div>
         </div>
